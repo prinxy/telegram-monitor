46dad59b2f3515744ec4416433e25f7789637201
Nnamdi <nnamdiibns@gmail.com>
2018-06-09 22:49:29 +0100

    Fix bug

--- monitor/management/commands/send_summary_link.py
+++ monitor/management/commands/send_summary_link.py
@@ -8,10 +8,8 @@ class Command(BaseCommand):
     help = 'Mails each recipient a link to the summary page.'
 
     def handle(self, *args, **options):
-        target_channels = TargetChannel.objects.all()
-        recipients = [r.email for r in Recipient.objects.filter(active=True)
-        message = 'Summary link for the last 24 Hours: https://ringindiadom.pythonanywhere.com/monitor/summary/ '
-
+        recipients = [r.email for r in Recipient.objects.filter(active=True)]
+        message = 'Summary link for the last 24 Hours: https://ringindiadom.pythonanywhere.com/monitor/summary/'
         send_mail(
             'Summary mail for {}'.format(datetime.now()),
             message,
