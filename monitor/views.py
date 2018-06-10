ee3c963d3b61cd0b44f8aac53656e031687a14fd
Nnamdi <nnamdiibns@gmail.com>
2018-06-10 19:33:17 +0100

    Remove day increase and decrease

--- monitor/views.py
+++ monitor/views.py
@@ -31,8 +31,8 @@ def search_dates(request, template='monitor/search_results.html'):
     if form.is_valid():
         start = form.cleaned_data['start_date']
         end = form.cleaned_data['end_date']
-        start = start - datetime.timedelta(days=1)
-        end = end + datetime.timedelta(days=1)
+        # start = start - datetime.timedelta(days=1)
+        # end = end + datetime.timedelta(days=1)
         pinned_messages = PinnedMessage.objects.select_related(
             'channel'
         ).filter(date_created__range=(start, end)).order_by('channel')
