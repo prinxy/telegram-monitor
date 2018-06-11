0428ee78974d672516fdab645a95ddbc58e38452
Nnamdi <nnamdiibns@gmail.com>
2018-06-11 21:50:25 +0100

    Add login code field to Config settings

--- monitor/models.py
+++ monitor/models.py
@@ -8,6 +8,7 @@ class Configuration(models.Model):
     api_hash = models.CharField(max_length=255)
     phone = models.CharField(max_length=50)
     username = models.CharField(max_length=255)
+    login_code = models.CharField(max_length=255, default='default')
     active = models.BooleanField(default=False)
 
     def __str__(self):
