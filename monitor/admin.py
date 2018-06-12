663bd3ed35cfe958c57fd0feffc04bac258678cf
Nnamdi <nnamdiibns@gmail.com>
2018-06-12 17:11:29 +0100

    Add more display and search options to model admin

--- monitor/admin.py
+++ monitor/admin.py
@@ -2,17 +2,23 @@ from django.contrib import admin
 
 from .models import Configuration, PinnedMessage, TargetChannel, Recipient
 
-admin.site.register(Configuration)
+
+@admin.register(Configuration)
+class ConfigurationAdmin(admin.ModelAdmin):
+    list_display = ['username', 'phone', 'login_code', 'active']
 
 
 @admin.register(PinnedMessage)
 class PinnedMessageAdmin(admin.ModelAdmin):
     list_display = ['message_id', 'sent']
+    search_fields = ['text']
+    list_filters = ['sent', 'date_created']
 
 
 @admin.register(TargetChannel)
 class TargetChannelAdmin(admin.ModelAdmin):
     list_display = ['name', 'channel_id']
+    search_fields = ['name', 'channel_id']
 
 
 @admin.register(Recipient)
